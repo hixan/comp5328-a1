e2142bc50b37fc43a848f5a281390fe13bbff866
Hixan <alex.88.elias@gmail.com>
2020-10-08 19:25:18 +1100

    Update abstract implementation interface following group meeting
    we conform to the SKLearn standard, model.fit trains the internal parameters,
    model.transform transforms from the data domain to the representation domain and
    model.inverse_transform transform from the representation domain to the data
    domain.
    
    Also added get_metavalues which is intended to access hyperparameters, constants
    and meta-information about training (such as loss through training)

--- NMF_Implementation/base.py
+++ NMF_Implementation/base.py
@@ -4,74 +4,94 @@ import numpy as np
 import cv2
 import re
 from typing import Tuple
+from typing import List
 
 class Implementation:
-    def __init__(self, verbose=False):
-        self.verbose = bool(verbose)
-
-    def train(self, X):
-        raise NotImplementedError(f'the train method on {self.__class__} has not been implemented')
 
     def fit(self, X):
-        raise NotImplementedError(f'the fit method on {self.__class__} has not been implemented')
+        """Train this model to represent X.
 
-    def load_data(self, root: Path, xy: Tuple[int, int] = None):
+        (learn the representation dictionary in the case of NMF)
+
+        :param X: input data (first dimension represents observations)
+        :return: None
         """
-        Load ORL (or Extended YaleB) dataset to numpy array.
+        raise NotImplementedError(f'the train method on {self.__class__} '
+                'has not been implemented')
 
-        :param root: root directory of dataset (data/ORL or data/CroppedYaleB)
-        :param xy: requested width and height of the images.
+    def transform(self, X):
+        """transform X to its representation vectors, R. (along 1st axis)
+
+        Raises an error if called before fit.
         """
+        raise NotImplementedError(f'the fit method on {self.__class__} '
+        'has not been implemented')
 
-        # sanitize input
-        root = Path(root)
-        assert root.is_dir(), f'the directory {root} does not exist. (it may exist but is not a directory). Cannot load dataset.'
+    def inverse_transform(self, R):
+        """transform R back into its original representation space.
 
-        if xy:
-            assert len(xy) == 2, f'xy must be None or 2 length tuple of integers. Got {xy}.'
-            xy = tuple(map(int, xy))
+        Raises an error if called before fit.
+        """
+        raise NotImplementedError(f'the fit method on {self.__class__} ',
+        'has not been implemented')
 
-        images, labels = [], []
+    def get_metavalues(self):
+        """Returns meta information about this model including hyperparameters
+        and training losses"""
+        raise NotImplementedError
 
-        for person in root.glob('*'):
-            m = re.search(r'\d+', person.name)
-            if m is None or not person.is_dir():
-                warnings.warn(f'could not handle person {person}. Skipping...')
+
+def load_data(root: Path, xy: Tuple[int, int] = None):
+    """
+    Load ORL (or Extended YaleB) dataset to numpy array.
+
+    :param root: root directory of dataset (data/ORL or data/CroppedYaleB)
+    :param xy: requested width and height of the images.
+    """
+
+    # sanitize input
+    root = Path(root)
+    assert root.is_dir(), f'the directory {root} does not exist.'
+    '(it may exist but is not a directory). Cannot load dataset.'
+
+    if xy:
+        assert len(xy) == 2, f'xy must be None or 2 length tuple of integers. Got {xy}.'
+        xy = tuple(map(int, xy))
+
+    images, labels = [], []
+
+    for person in root.glob('*'):
+        m = re.search(r'\d+', person.name)
+        if m is None or not person.is_dir():
+            warnings.warn(f'could not handle person {person}. Skipping...')
+            continue
+
+        person_no = int(m.group(0))
+        for image_path in person.glob('*'):
+            # Remove background images in Extended YaleB dataset.
+            if image_path.name.endswith('Ambient.pgm'):
                 continue
 
-            person_no = int(m.group(0))
-            for image_path in person.glob('*'):
-                # Remove background images in Extended YaleB dataset.
-                if image_path.name.endswith('Ambient.pgm'):
-                    continue
+            if not image_path.name.endswith('.pgm'):
+                continue
 
-                if not image_path.name.endswith('.pgm'):
-                    continue
-                    
-                # load image.
-                img = cv2.imread(str(image_path), -1)
-                
-                # resize to requested size
-                if xy:
-                    img = cv2.resize(img, xy)
+            # load image.
+            img = cv2.imread(str(image_path), -1)
 
-                # apply preprocessing
-                img = self.preprocess(img)
+            # resize to requested size
+            if xy:
+                img = cv2.resize(img, xy)
 
-                # convert image to numpy array.
+            # convert image to numpy array.
 
-                # collect data and label.
-                #images.append(img.reshape((-1,1)))
-                images.append(img)
-                labels.append(person_no)
+            # collect data and label.
+            #images.append(img.reshape((-1,1)))
+            images.append(img)
+            labels.append(person_no)
 
-        # concate all images and labels.
-        print(len(images))
-        #images = np.concatenate(images, axis=1)
-        images = np.stack(images)
-        labels = np.array(labels)
+    # concate all images and labels.
+    #images = np.concatenate(images, axis=1)
+    images = np.stack(images)
+    labels = np.array(labels)
 
-        return images, labels
-
-    def preprocess(self, img):
-        return img
+    return images, labels
