4c5229cbc893ae3e3a64083c8de02a4fbee77935
Hixan <alex.88.elias@gmail.com>
2020-10-19 08:07:57 +1100

    added a few extra tests for L21Norm

--- tests/test_l21norm.py
+++ tests/test_l21norm.py
@@ -3,6 +3,7 @@ from NMF_Implementation.base import load_data
 from matplotlib import pyplot as plt
 import numpy as np
 
+
 def test_fit():
     alg = L21Norm.Algorithm(15)
     X, Y = load_data('data/ORL')
@@ -18,3 +19,29 @@ def test_fit():
         plt.imshow(c)
         plt.show()
     
+
+def random(k, n, p, tolerance=None):
+    k = 15
+    n = 300
+    p = 30
+
+    print(f"k={10}\nn={300}\np={15}")
+    alg = L21Norm.Algorithm(k)
+    data = np.random.randint(0, 255, (n, p))
+    alg.fit(data)
+    assert alg.transform(data).shape == (n, k)
+    assert alg.inverse_transform(np.random.randint(0, 255, (2*n, k))).shape == (2*n, k)
+
+    if tolerance is not None:
+        # max deviation
+        maxdev = np.max(alg.inverse_transform(alg.transform(data)) - data)
+        print('Maximum difference', maxdev)
+        assert maxdev <= tolerance
+
+
+def test_random_shapes():
+    random(2, 50, 8)
+
+
+def test_random_values():
+    random(8, 50, 8, 0.01)
