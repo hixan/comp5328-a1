d089d5d006444b717adb9ef46bcd7d7818e2f615
Hixan <alex.88.elias@gmail.com>
2020-10-19 09:03:09 +1100

    make tests a module and ignore its installation

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/hixan/comp5328-a1",
-    packages=setuptools.find_packages(),
+    packages=['NMF_Implementation'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
